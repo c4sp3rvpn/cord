0f04bd734db2fb263fcb58561246eeec22eb0dc8
c4sp3rvpn <v.kozlov@rublevski.by>
2015-12-08 12:35:23 +0300

    1
    1

---
+++ Phonegap-HelloWorld/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-yandexnavi/www/yandexnavi.js",
+        "id": "cordova-plugin-yandexnavi.Yandexnavi",
+        "pluginId": "cordova-plugin-yandexnavi",
+        "clobbers": [
+            "Yandexnavi"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
